47d727eb0a8822d3a19f4ff1f78c9b06c469fa14
Rahimi <r4himi.10@gmail.com>
2026-04-08 10:29:14 +0430

    updated

--- yourdoctor/settings.py
+++ yourdoctor/settings.py
@@ -149,7 +149,7 @@ CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Kabul'
+CELERY_TIMEZONE = 'europe/stockholm'
 
 import os
 
